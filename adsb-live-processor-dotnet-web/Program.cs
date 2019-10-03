03ee4de8b2ee60ff5b832dcf6434c066f7165d82
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-10-03 09:53:18 -0400

    Refactor processor as webapp

---
+++ adsb-live-processor-dotnet-web/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging;
+
+namespace adsb_live_processor_dotnet_web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .AddCloudFoundry()
+                .ConfigureLogging(cfg =>
+                {
+                    cfg.ClearProviders();
+                    cfg.AddDynamicConsole();
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ adsb-live-processor-dotnet-web/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.RabbitMQ;
+using Steeltoe.CloudFoundry.Connector.Redis;
+using Steeltoe.Management.CloudFoundry;
+
+namespace adsb_live_processor_dotnet_web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddRabbitMQConnection(Configuration, ServiceLifetime.Singleton);
+            services.AddRedisConnectionMultiplexer(Configuration);
+            services.AddCloudFoundryActuators(Configuration);
+            services.AddSingleton<FlightProcessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FlightProcessor processor)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseCloudFoundryActuators();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            
+            processor.Start();
+        }
+    }
+}
